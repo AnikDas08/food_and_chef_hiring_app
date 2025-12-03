85855a7c112f6bbba4ee837259b76e879e1fbb26
MD Naim Mia <developernaimul00@gmail.com>
2025-12-03 18:09:04 +0600

    update

--- lib/utils/constants/app_colors.dart
+++ lib/utils/constants/app_colors.dart
@@ -5,7 +5,6 @@ class AppColors {
   static const Color primaryColor = Colors.blue;
   static const Color secondary = Color(0xff454545);
   static const Color background = Color(0xFFFAFAFA);
-
   static const Color transparent = Colors.transparent;
   static const Color white = Colors.white;
   static const Color black = Colors.black;
