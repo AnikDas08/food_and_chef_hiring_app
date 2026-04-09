b8cd0ca710043a0dea28c629fc0cfe21b3495afe
Huzayfa Sani <huzaifa0133@gamil.com>
2026-04-09 13:01:29 +0600

    update n-15

--- lib/features/common/auth/signup_chef/presentation/screen/Chef_add_menu_items_screen.dart
+++ lib/features/common/auth/signup_chef/presentation/screen/Chef_add_menu_items_screen.dart
@@ -190,9 +190,7 @@ class CafeAddMenuItemsScreen extends StatelessWidget {
               padding: EdgeInsets.only(bottom: 20.h),
               child: TextButton(
                 onPressed: (){
-
                   Get.offAllNamed(AppRoutes.chefHomeScreen);
-
                 },
                 child: Text("Skip For Now",
                     style: TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500, color: const Color(0xFF272727))),
